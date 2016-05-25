c393fab8787b213b71df5140b823aa7c88ccd632
gxsham <gxsham@gmail.com>
2016-05-25 20:12:12 +0300

    changes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,21 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Collections.ObjectModel;
 using System.Net.Sockets;
 using System.Net;
-using System.Windows.Controls.Primitives;
 
 namespace lab5PR
 {
@@ -54,30 +44,30 @@ namespace lab5PR
 
 		private void Normal_Status_Checked(object sender, RoutedEventArgs e)
 		{
-			string name = Old_name.Text;
-			string code = "\000";
-			change_status(name,code);
+			string pattern = "D" + Old_name.Text + "\000";
+			send_pattern(pattern);
 		}
 
 		private void DND_Status_Checked(object sender, RoutedEventArgs e)
 		{
-			string name = Old_name.Text;
-			string code = "\010";
-			change_status(name,code);
+			string pattern = "D" + Old_name.Text + "\010";
+			send_pattern(pattern);
 		}
 
 		private void Away_Status_Checked(object sender, RoutedEventArgs e)
 		{
-			string name = Old_name.Text;
-			string code = "\020";
-			change_status(name,code); 
-
+			string pattern = "D"+ Old_name.Text + "\020";
+			send_pattern(pattern); 
+		}
+		private void Offline_Status_Checked(object sender, RoutedEventArgs e)
+		{
+			string pattern = "D" + Old_name.Text + "\030";
+			send_pattern(pattern);
 		}
 
-		private void change_status(string name,string code)
+		private void send_pattern(string pattern)
 		{
-			string text = "D" + name + code;
-			byte[] send_buffer = Encoding.ASCII.GetBytes(text);
+			byte[] send_buffer = Encoding.ASCII.GetBytes(pattern);
 			sock.SendTo(send_buffer, endPoint);
 		}
 
@@ -86,6 +76,7 @@ namespace lab5PR
 			Normal.Visibility = Visibility.Visible;
 			Private.Visibility = Visibility.Collapsed;
 			Channel.Visibility = Visibility.Collapsed;
+
 		}
 
 		private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
@@ -104,37 +95,62 @@ namespace lab5PR
 
 		private void Topic_KeyDown(object sender, KeyEventArgs e)
 		{
-			if(e.Key == Key.Enter)
+			if (e.Key == Key.Enter && Topic.Text!="")
 			{
-				sock.SendTo(Encoding.ASCII.GetBytes("B" + Topic.Text + "\0"), endPoint);
+				string pattern = "B" + Topic.Text + "\0";
+				send_pattern(pattern);
 			}
 		}
 
 		private void Old_name_KeyDown(object sender, KeyEventArgs e)
 		{
-			if(e.Key == Key.Enter)
+			if (e.Key == Key.Enter && Old_name.Text != "")
 			{
 				global_name.Add(Old_name.Text);
-				string text = "3" + global_name.ElementAt(0) + "\0" + global_name.ElementAt(1) + "\00";
-				byte[] send_buffer = Encoding.ASCII.GetBytes(text);
-				sock.SendTo(send_buffer, endPoint);
+				string pattern = "3" + global_name.ElementAt(0) + "\0" + global_name.ElementAt(1) + "\00";
+				send_pattern(pattern);
 				global_name.RemoveAt(0);
 			}
 		}
 
-		private void New_Name_Click(object sender, RoutedEventArgs e)
+		private void InputField_KeyDown(object sender, KeyEventArgs e)
 		{
-
+			if (e.Key == Key.Enter && InputField.Text != "")
+			{
+				string pattern = actual_pattern();
+				send_pattern(pattern);
+				InputField.Clear();
+			}
 		}
 
+		private string actual_pattern()
+		{
+			if(Private_radio.IsChecked==true)
+			{
+				return "6"+private_my_name.Text+"\0"+private_send_to.Text+"\0"+InputField.Text+"\0";
+			}
+			else if (Channel_radio.IsChecked==true)
+			{
+				return "2#"+Channel_name.Text+"\0" + Old_name.Text + "\0" + InputField.Text+"\0";
+			}
+			else
+			{
+				return "2#Main\0" + Old_name.Text + "\0" + InputField.Text+"\0";
+			}
+		}
 
+		private void Button_Join(object sender, RoutedEventArgs e)
+		{
+			string pattern = "4" + Old_name.Text + "\0#" + Channel_name.Text + "\030";
+			send_pattern(pattern);
+		}
 
-		//private void Status_name_KeyDown(object sender, KeyEventArgs e)
-		//{
-		//	string name = Status_name.Text;
-		//	string text = "3" + name + "\0" + "newnameblin" + "\00";
-		//	byte[] send_buffer = Encoding.ASCII.GetBytes(text);
-		//	sock.SendTo(send_buffer, endPoint);
-		//}
+		private void Button_Leave(object sender, RoutedEventArgs e)
+		{
+			string pattern = "5" + Old_name.Text + "\0#" + Channel_name.Text + "\00";
+			send_pattern(pattern);
+		}
+
+		
 	}
 }
